412b511c5a7b80bcf4bd185efe70ccba392e7c77
xjhan17 <71535733+xjhan17@users.noreply.github.com>
2020-10-16 21:20:41 -0400

    add p5

---
+++ p5.py
@@ -0,0 +1,16 @@
+from z3 import *
+
+ina0, inb0, outa0, outb0
+
+phia = And((outa0=ina0),(outa1=outa0*ina0), (outa2=outa1*ina0))
+phib = (outb0 = (inb0*inb0)*inb0)
+
+# p -> q equiv \neg q \or q
+phi = Or(Or(Not(ina0=inb0), Not(phia), Not(phib)), (outa0=outb0))
+
+
+
+s = Solver()
+s.add(Not(phi))
+
+print(s.check())
