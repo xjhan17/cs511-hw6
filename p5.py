66ad88e4e3de4e25536db361f177b352e3b91274
xjhan17 <71535733+xjhan17@users.noreply.github.com>
2020-10-16 21:33:46 -0400

    update p5

--- p5.py
+++ p5.py
@@ -6,7 +6,7 @@ phia = And((outa0=ina0),(outa1=outa0*ina0), (outa2=outa1*ina0))
 phib = (outb0 = (inb0*inb0)*inb0)
 
 # p -> q equiv \neg q \or q
-phi = Or(Or(Not(ina0=inb0), Not(phia), Not(phib)), (outa0=outb0))
+phi = Or(Or(Not(ina0=inb0), Not(phia), Not(phib)), (outa2=outb0))
 
 
 
